913920384564a0d50a0f11a511aee42a071b0487
Ayo crescent <70281852+anayo-hub@users.noreply.github.com>
2020-10-18 10:38:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.nav__btn').addEventListener('click',shownav);
+
+
+
+function shownav (){
+    // alert('clicked');
+ document.querySelector('.nav__links').classList.toggle('show__links'); 
+}
